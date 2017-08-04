0771e26c35910e9f43715e436e73c08a85cd5ec7
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2017-08-04 12:01:18 +0900

    [WIP] add setup.py but could not be installed

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+install_requirements = [
+    'six>=1.9.0',
+    'numpy>=1.9.0',
+    'pillow>=4.2.0'
+]
+
+setup(
+    name='pyrtools',
+    version='0.0.1',
+    description="tools for multi-scale image processing",
+    author='Eero Simoncelli, Rob Young, Aiga SUZUKI',
+    author_email='ai-suzuki@aist.go.jp',
+    license='MIT License',
+    setup_requires=[],
+    install_requires=install_requirements,
+)
